96ed33eba5481c5255bb92e901e6d60b2ce48e9a
lardedah <tomholbrook94@gmail.com>
2024-01-21 22:06:55 +0000

    delete dead weight

--- discover_services.py
+++ discover_services.py
@@ -18,12 +18,3 @@ for r in range(1, 1000000):
 
 cur.close()
 conn.close()
-
-
-
-
-{'id': '5116', 'label': 'http://dd.eionet.europa.eu/vocabulary/aq/pollutant/9 9912 - Hafod-yr-ynys Hill Roadside-Nitrogen oxides (air), Hafod-yr-ynys Hill Roadside-Nitrogen oxides (air)', 'uom': 'ug.m-3', 'station': 
-{'properties': 
-{'id': 787424, 'label': 'Hafod-yr-ynys Hill Roadside-Nitrogen oxides (air)'}, 'geometry': {'coordinates': 
-[51.680493, -3.1343249998968368, 'NaN'], 'type': 'Point'}, 'type': 'Feature'}},
-
